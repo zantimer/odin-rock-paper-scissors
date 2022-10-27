9f1f74bfcf2988b4955411d92db228bde6dcfc03
zantimer <karol.nikiel1@gmail.com>
2022-10-27 11:07:44 +0100

    wasted 5 commits due to wrong cloning method - lesson learned

---
+++ script.js
@@ -0,0 +1,34 @@
+// 1. Computer picks randomly rock, paper or scissors
+// 2. Computer stores it's pick, waits for player input
+// 3. Player types in rock, paper, or scissors as his choice
+// 4. Player input gets stored
+// 5. Both AI and Player inputs are compared and stored to winner and loser
+// 6. Write to paragraph: [winner-pick] beats [loser-pick], and You Won, or You Lost
+
+let computerChoice;
+let playerChoice;
+const pickTable = ["rock", "paper", "scissors"];
+
+function computerPick() {
+    let rollPick = Math.floor(Math.random() * 3);
+    computerChoice = "" + pickTable[rollPick];
+    console.log("Computer choice: "+computerChoice);
+}
+function playerPick() {
+    
+}
+function playRound(playerInput, computerInput) {
+    for(let i = 0; i<= pickTable.length -1 ; i++){
+        if (pickTable[i] == document.querySelector("input").value){
+            playerChoice = pickTable[i];
+            console.log("Player choice: "+playerChoice);
+        }
+    }
+    
+    computerInput = computerPick();
+    // take player choice and computer choice, compare and return winner
+    let winner;
+    if (playerInput==computerInput) {
+        console.log("works");
+    }
+}
