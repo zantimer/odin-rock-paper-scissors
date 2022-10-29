57d8467e68de217fcf0feb90d99a3b04ced67279
zantimer <karol.nikiel1@gmail.com>
2022-10-29 11:10:28 +0100

    refactored to DOM methods and button UI

--- script.js
+++ script.js
@@ -7,86 +7,101 @@
 
 let AIScore = 0;
 let PlayerScore = 0;
+let winner = "";
+let para = document.createElement('p');
+
 const pickTable = ["rock", "paper", "scissors"];
+const buttons = document.querySelectorAll('.playBtn');
+const resultDiv = document.querySelector('div');
+resultDiv.appendChild(para);
+
+buttons.forEach(button => {
+    button.addEventListener('click', 
+    playRound);
+});
+
+function writeResult (msg) {
+    para.textContent += "\n"+msg;
+}
 
 function computerPick() {
     let rollPick = Math.floor(Math.random() * 3);
     //computerChoice = "" + pickTable[rollPick];
-    console.log("Computer choice: "+pickTable[rollPick]);
+    writeResult("Computer choice: "+pickTable[rollPick]);
     return "" + pickTable[rollPick];
-}
-function playerPick() {
-    return prompt("Type in your pick!")
 }
 
 function playRound(playerInput, computerInput) {
     //for(let i = 0; i<= pickTable.length -1 ; i++){
         //if (pickTable[i] == document.querySelector("input").value){
            // playerInput = pickTable[i];
-           // console.log("Player choice: "+playerInput);
+           // writeResult("Player choice: "+playerInput);
         //}
     //}
-    playerInput = playerPick().toLowerCase();
-    console.log("Player choice: "+playerInput);
+    para.textContent = "";
+    playerInput = this.textContent;
+    writeResult("Player choice: "+playerInput);
     computerInput = computerPick();
     // take player choice and computer choice, compare and return winner
     
     if (playerInput != "rock") {
         if (playerInput != "scissors") {
             if (playerInput != "paper"){
-                console.log("Wrong input, type in rock, paper, or scissors");
+                writeResult("Wrong input, type in rock, paper, or scissors");
             }
         }
+    }
+    if (AIScore == 5)
+    {
+        winner = "AI"
+        writeResult("YER LOST, reset if you want to go again");
+    }
+    else if (PlayerScore == 5)
+    {
+        winner = "Player"
+        writeResult("YA WON, reset if you want to go again");
+    }
+    if (winner == "")
+    {
+    if (playerInput==computerInput) {
+        writeResult("Draw!");
         
     }
-    if (playerInput==computerInput) {
-        console.log("Draw!");
-    }
     else if (playerInput=="rock" && computerInput=="paper") {
-        console.log("You Lost This Round!");
+        writeResult("You Lost This Round!");
         AIScore++;
+        
     }
     else if (playerInput=="rock" && computerInput=="scissors") {
-        console.log("You Won This Round!");
+        writeResult("You Won This Round!");
         PlayerScore++;
+        
     }
     else if (playerInput=="paper" && computerInput=="scissors") {
-        console.log("You Lost This Round!");
+        writeResult("You Lost This Round!");
         AIScore++;
+        
     }
     else if (playerInput=="paper" && computerInput=="rock") {
-        console.log("You Won This Round!");
+        writeResult("You Won This Round!");
         PlayerScore++;
+        
     }
     else if (playerInput=="scissors" && computerInput=="paper") {
-        console.log("You Won This Round!");
+        writeResult("You Won This Round!");
         PlayerScore++;
+        
     }
     else if (playerInput=="scissors" && computerInput=="rock") {
-        console.log("You Lost This Round!");
+        writeResult("You Lost This Round!");
         AIScore++;
+        
     }
-    console.log("Computer score: "+AIScore+" Player score: "+PlayerScore);
+    writeResult("Computer score: "+AIScore+" Player score: "+PlayerScore);
+    }
 }
 function resetScores() {
 AIScore = 0;
 PlayerScore = 0;
 }
-function playGame() {
-    for (let i = 0; i <= 4; i++ ){
-        playRound();
-    }
-    let winner;
-    if (PlayerScore > AIScore) {
-        winner = "YOU WON!";
-    }
-    else if (PlayerScore < AIScore) {
-        winner = "YOU LOST!";
-    }
-    else if (PlayerScore == AIScore){
-        winner = "ITS A DRAW!";
-    }
-    let writeWinnerHere = document.querySelector('p');
-    writeWinnerHere.textContent = winner;
-    resetScores();
-}
+
